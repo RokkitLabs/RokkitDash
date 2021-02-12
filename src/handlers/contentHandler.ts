6753a03d64e4e1b44b7919f7443898bc245e3fae
TatoExp <mwmatthew10@gmail.com>
2021-02-12 13:02:12 +0000

    backup commit

--- src/handlers/contentHandler.ts
+++ src/handlers/contentHandler.ts
@@ -1,45 +1,44 @@
 import express from "express";
 
+
 export class ContentHandler {
-    private _config : any;
-    private _documentRoot = "panel";
-    private _app = express();
+	private documentRoot = "panel";
+	private app = express();
 
-    constructor(config : any) {
-        this._config = config;
-        if(!config) 
-        {
-            this._documentRoot = "install";
-            this._config = {};
-        }
+	constructor(
+		private config: any
+	) {
+		if(!config) 
+		{
+			this.documentRoot = "install";
+			this.config = {};
+		}
 
-        this._app.set('view engine', 'ejs');
-        this._app.set("views", "./views");
-    }
+		this.app.set('view engine', 'ejs');
+		this.app.set("views", "./views");
+	}
 
-    start = async() => {
-        
-        this._app.use(express.static("static"));
-        
-        this._app.get("/panel", (req, res) => { //if navigated to the root
-            res.render(`${this._documentRoot}/index`);
-        })
+	async start() {
+		this.app.use(express.static("static"));
+		
+		this.app.get("/panel", (req, res) => { //if navigated to the root
+			res.render(`${this.documentRoot}/index`);
+		});
 
-        this._app.get("/panel/(/*)?", (req, res) => {
-            let path = req.path;
-            if(path == "/panel/") { //If its /panel then go to index
-                res.render(`${this._documentRoot}/index`);
-            }
-            else
-            {
-                path = path.replace("/panel", "");
-                res.render(`${this._documentRoot}${path}`);
-            }
-            res.send(req.path);
-        });
+		this.app.get("/panel/(/*)?", (req, res) => {
+			let path = req.path;
+			if(path == "/panel/") { //If its /panel then go to index
+				res.render(`${this.documentRoot}/index`);
+			}
+			else
+			{
+				path = path.replace("/panel", "");
+				res.render(`${this.documentRoot}${path}`);
+			}
+			res.send(req.path);
+		});
 
-
-        this._app.listen(3000);
-        console.info(`Panel listening in "${this._documentRoot}" mode`);
-    }
+		this.app.listen(3000);
+		console.info(`Panel listening in "${this.documentRoot}" mode`);
+	}
 }
--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,31 @@
 import fs from "fs";
 import ini from "ini";
-import {ContentHandler} from "./handlers/contentHandler"
+import {ContentHandler} from "./handlers/contentHandler";
+
 
 (async () => {
-    let config;
+	let config;
 
-    try {
-        await fs.promises.mkdir("/etc/rokkitdash", {recursive: true});
-    }
-    catch {
-        return console.error("Error occured creating /etc/rokkitdash");
-    }
+	try {
+		await fs.promises.mkdir("/etc/rokkitdash", {recursive: true});
+	}
+	catch {
+		return console.error("Error occured creating /etc/rokkitdash");
+	}
 
-    try{
-        let content = await fs.promises.readFile("/etc/rokkitdash/config.ini", "utf-8"); //Read config
-        config = ini.parse(content);
-    }
-    catch {}
+	try{
+		let content = await fs.promises.readFile("/etc/rokkitdash/config.ini", "utf-8"); //Read config
+		config = ini.parse(content);
+	}
+	catch {}
 
-    try {
-        let handler = new ContentHandler(config); //Create the handler
-        handler.start();
-    }
-    catch {
-        return console.error("An error occured starting the ContentHandler for the panel");
-    }
+	try {
+		let handler = new ContentHandler(config); //Create the handler
+		handler.start();
+	}
+	catch {
+		return console.error("An error occured starting the ContentHandler for the panel");
+	}
 
 })();
 
